e00e553aaf4541616bcf5ca143ea03c709aaa81f
Sorin Peste <neaorin@gmail.com>
2017-07-27 10:49:37 +0300

    naive strength of schedule + neural net

--- WorldCup.R
+++ WorldCup.R
@@ -26,6 +26,9 @@ install.packages("https://cran.rstudio.com/bin/windows/contrib/3.4/coin_1.2-1.zi
                 repos = NULL, type="source") 
 library(coin) # Conditional Inference Procedures
 
+install.packages("neuralnet")
+library(neuralnet) # Neural networks
+
 install.packages("Metrics")
 library(Metrics) # Eval metrics for ML
 
@@ -41,6 +44,13 @@ if(file.exists("matches.csv")) matches_original <- read_csv("matches.csv")
 #eliminate any duplicates
 matches <- matches_original %>%
   distinct(.keep_all = TRUE, date, team1, team2)
+
+temp <- matches %>%
+  group_by(date, team1, team2) %>%
+  summarise(
+    count = n()
+  ) %>%
+  filter(count > 1) 
 
 #format datetime
 matches$date <- as.POSIXct(strptime(matches$date, "%Y%m%d"), origin="1960-01-01", tz="UTC")
@@ -61,8 +71,6 @@ matches$team1Home <- mapply(grepl, pattern=matches$team1Text, x=matches$venue, M
 matches$team2Home <- mapply(grepl, pattern=matches$team2Text, x=matches$venue, MoreArgs = list(fixed = TRUE, ignore.case = FALSE))
 matches$neutralVenue <- !(matches$team1Home | matches$team2Home)
 
-temp <- matches %>% select(date, team1Text, team2Text, venue, team1Home, team2Home, neutralVenue)
-
 # text-matching the venue is not 100% accurate.
 # some games get TRUE for both team1 and team2 (ex. Congo DR vs Congo)
 # in this case, team1 is at home
@@ -78,6 +86,9 @@ matches$qualifier[matches$CupName %like% "Qual"] <- TRUE
 matches$finaltourn <- FALSE
 matches$finaltourn[matches$CupName %like% "Final"] <- TRUE
 matches$finaltourn[matches$CupName %like% "Confederations Cup"] <- TRUE
+
+# only use official matches (no friendlies)
+matches <- matches %>% filter(friendly == FALSE)
 
 # transform the matches table into a team performance table, where each team being 
 # involved in a game is a separate observation (row)
@@ -150,16 +161,22 @@ matches %>%
   )
 
 # how many goals have been scored per game over the years? 
-matches %>%
+# how about the win margin (goal difference) over the years?
+history_indicators <- matches %>%
   group_by(year = year(date)) %>%
   summarize(
-    totalgames = n(),
-    totalgoals = sum(team1Score + team2Score),
-    goalspergame = totalgoals / totalgames
-    ) %>%
-  ggplot(mapping = aes(x = year, y = goalspergame)) +
+    gs_pergame = mean(team1Score + team2Score),
+    gd_pergame = mean(abs(team1Score - team2Score))
+  ) %>%
+  dplyr::select (year, gs_pergame, gd_pergame)
+
+ggplot(history_indicators, mapping = aes(x = year, y = gs_pergame)) +
     geom_point() +
     geom_smooth(method = "loess")
+
+ggplot(history_indicators, mapping = aes(x = year, y = gd_pergame)) +
+  geom_point() +
+  geom_smooth(method = "loess")
 
 # Out of the teams who have played at least 300 games, what are the winning percentages for each of those teams?
 # We use the current point system of 3 points for a win, 1 point for a draw, 0 for a loss.
@@ -195,6 +212,9 @@ countryCodeMappings <- matrix(c(
 for (i in 1:nrow(countryCodeMappings)) {
   teamperf$name[teamperf$name == countryCodeMappings[i,1]] <- countryCodeMappings[i,2]
   teamperf$opponentName[teamperf$opponentName == countryCodeMappings[i,1]] <- countryCodeMappings[i,2]
+  
+  matches$team1[matches$team1 == countryCodeMappings[i,1]] <- countryCodeMappings[i,2]
+  matches$team2[matches$team2 == countryCodeMappings[i,1]] <- countryCodeMappings[i,2]
 }
 
 # let's run the win percentage graph again
@@ -207,7 +227,18 @@ plot_winpercentage(teamperf %>% filter(friendly == FALSE), 150)
 # how about final tournaments only?
 # the number of games played in final tournaments is much smaller than friendlies or qualifiers.
 
-plot_winpercentage(teamperf %>% filter(finaltourn == TRUE), 30)
+plot_winpercentage(teamperf %>% filter(finaltourn == TRUE), 10)
+
+perf_in_finals <- teamperf %>% 
+  filter(finaltourn == TRUE) %>%
+  group_by(name) %>%
+  summarize(
+    totalgames = n(),
+    wins = length(w[w==TRUE]) + length(pen_w[pen_w==TRUE]),
+    draws = length(d[d==TRUE]),
+    winpercentage = (wins*3 + draws) / (totalgames*3)
+  ) %>%
+  filter(totalgames >= 10)
 
 # what is the occurence frequency for match scores?
 
@@ -244,78 +275,81 @@ lagfn_per <- function(data, width) {
   return (lagfn(data, width) / width)
 }
 
-features <- teamperf %>%
+opp_strength <- function(teams) {
+  return (mean((perf_in_finals %>% filter(name %in% teams))$winpercentage))
+}
+
+
+team_features <- teamperf %>%
   arrange(name, date) %>%
   group_by(name) %>%
   mutate(
-    last5games_w = lagfn(w, 5),
-    last20games_w = lagfn(w, 20),
-    last35games_w = lagfn(w, 35),
+    last5games_w_per = lagfn_per(w, 5),
+    last20games_w_per = lagfn_per(w, 20),
+    last35games_w_per = lagfn_per(w, 35),
 
-    last5games_l = lagfn(l, 5),
-    last20games_l = lagfn(l, 20),
-    last35games_l = lagfn(l, 35),
-        
+    last5games_l_per = lagfn_per(l, 5),
+    last20games_l_per = lagfn_per(l, 20),
+    last35games_l_per = lagfn_per(l, 35),
+
+    last5games_d_per = lagfn_per(d, 5),
+    last20games_d_per = lagfn_per(d, 20),
+    last35games_d_per = lagfn_per(d, 35),
+            
     last5games_gd_per = lagfn_per(gd, 5),
     last20games_gd_per = lagfn_per(gd, 20),
-    last35games_gd_per = lagfn_per(gd, 35)
+    last35games_gd_per = lagfn_per(gd, 35),
+    
+    strength_of_schedule = rollapplyr(opponentName, width = 21, FUN = opp_strength, fill = NA, partial=TRUE)
   ) %>%
   dplyr::select (
     match_id, date, name, opponentName, gs, ga,
-    w, last5games_w, last20games_w, last35games_w,
-    l, last5games_l, last20games_l, last35games_l,
-    gd, last5games_gd_per, last20games_gd_per, last35games_gd_per
+    w, last5games_w_per, last20games_w_per, last35games_w_per,
+    l, last5games_l_per, last20games_l_per, last35games_l_per,
+    d, last5games_d_per, last20games_d_per, last35games_d_per,
+    gd, last5games_gd_per, last20games_gd_per, last35games_gd_per,
+    strength_of_schedule
           ) %>%
   ungroup()
+
+# change any missing data to 0 for strength of schedule
+team_features$strength_of_schedule[is.na(team_features$strength_of_schedule)] <- 0
 
 head(features, n = 50)
 summary(features)
 
-# calculate the match outcome as a value inside (0,1)
-# arrange the possible outcomes so that, the closer the outcome is to either 0 or 1, the more lopsided the win/loss was
-# and the closer to 0.5, the more tight the result, and the chance of a draw
-
-scorefreq <- scorefreq %>%
-  arrange(
-    team1Score - team2Score,
-    (team1Score + team2Score) * sign(team1Score - team2Score)
-  ) %>%
-  within(outcome <- cumsum(freq))
-
-scorefreq %>%
-  filter(freq >= 0.001) %>%
-  ggplot(mapping = aes(x = reorder(scoretext, outcome), y = outcome)) +
-  geom_point() +
-  theme(axis.text.x = element_text(angle = 60, hjust = 1))
-
-# outcome is skewed to the left (draw values are between 0.26 and 0.49) because the home team is more likely to win
-scorefreq %>% filter(team1Score - team2Score == 0)
-
-
 # fold per-team features into per-match features
 match_features <- matches %>%
-  left_join(features, by=c("match_id", "team1" = "name")) %>%
-  left_join(features, by=c("match_id", "team2" = "name"), suffix=c(".t1",".t2")) %>%
+  left_join(team_features, by=c("match_id", "team1" = "name")) %>%
+  left_join(team_features, by=c("match_id", "team2" = "name"), suffix=c(".t1",".t2")) %>%
   dplyr::select(
     date, match_id, team1, team2, team1Home, team2Home, neutralVenue, team1Score, team2Score, friendly, qualifier, finaltourn,
-    last5games_w.t1,
-    last20games_w.t1,
-    last35games_w.t1,
-    last5games_l.t1,
-    last20games_l.t1,
-    last35games_l.t1,
+    last5games_w_per.t1,
+    last20games_w_per.t1,
+    last35games_w_per.t1,
+    last5games_l_per.t1,
+    last20games_l_per.t1,
+    last35games_l_per.t1,
+    last5games_d_per.t1,
+    last20games_d_per.t1,
+    last35games_d_per.t1,
     last5games_gd_per.t1, 
     last20games_gd_per.t1,
     last35games_gd_per.t1,
-    last5games_w.t2,
-    last20games_w.t2,
-    last35games_w.t2,
-    last5games_l.t2,
-    last20games_l.t2,
-    last35games_l.t2,
+    strength_of_schedule.t1,
+    last5games_w_per.t2,
+    last20games_w_per.t2,
+    last35games_w_per.t2,
+    last5games_l_per.t2,
+    last20games_l_per.t2,
+    last35games_l_per.t2,
+    last5games_d_per.t2,
+    last20games_d_per.t2,
+    last35games_d_per.t2,
     last5games_gd_per.t2, 
     last20games_gd_per.t2,
-    last35games_gd_per.t2
+    last35games_gd_per.t2,
+    strength_of_schedule.t2
   )
 
 # add the outcome column (value to train / predict on)
@@ -342,7 +376,7 @@ match_features %>% filter(date >= "2014-06-12" & date <= "2014-07-13" & finaltou
 
 # create the training formula 
 trainformula <- as.formula(paste('outcome',
-                                 paste(names(match_features)[c(2:25)],collapse=' + '),
+                                 paste(names(match_features)[c(2:33)],collapse=' + '),
                                  sep=' ~ '))
 trainformula
 
@@ -350,17 +384,8 @@ trainformula
 
 # training and testing datasets
 
-data.train1 <- match_features %>% filter(date >= '1960/1/1' & date < '2008/1/1')
-data.test1 <- match_features %>% filter(date >= '2008/1/1')
-
-data.train2 <- match_features %>% filter(date >= '1960/1/1' & date < '2003/1/1')
-data.test2 <- match_features %>% filter(date >= '2003/1/1')
-
-data.train3 <- match_features %>% filter(date >= '1960/1/1' & date < '1999/1/1')
-data.test3 <- match_features %>% filter(date >= '1999/1/1')
-
-data.train4 <- match_features %>% filter(date >= '1960/1/1' & date < '1993/1/1')
-data.test4 <- match_features %>% filter(date >= '1993/1/1')
+data.train1 <- match_features %>% filter(date >= '1960/1/1' & date < '2010/1/1')
+data.test1 <- match_features %>% filter(date >= '2010/1/1')
 
 # Random forest
 
@@ -368,88 +393,75 @@ set.seed(4342)
 
 model.randomForest1 <- randomForest::randomForest(trainformula, data = data.train1, 
                                                   importance = TRUE, ntree = 500)
-model.randomForest2 <- randomForest::randomForest(trainformula, data = data.train2, 
-                                                  importance = TRUE, ntree = 50)
-model.randomForest3 <- randomForest::randomForest(trainformula, data = data.train3, 
-                                                  importance = TRUE, ntree = 50)
-model.randomForest4 <- randomForest::randomForest(trainformula, data = data.train4, 
-                                                  importance = TRUE, ntree = 50)
 
 randomForest::varImpPlot(model.randomForest1)
 
 data.pred.randomForest1 <- predict(model.randomForest1, data.test1)
-data.pred.randomForest2 <- predict(model.randomForest2, data.test2)
-data.pred.randomForest3 <- predict(model.randomForest3, data.test3)
-data.pred.randomForest4 <- predict(model.randomForest4, data.test4)
 
 # Conditional inference trees
 
 set.seed(4342)
 
 model.condForest1 <- party::cforest(trainformula, data = data.train1, 
-                                    controls = party::cforest_unbiased(ntree = 500, mtry = 7))
-model.condForest2 <- party::cforest(trainformula, data = data.train2, 
-                                    controls = party::cforest_unbiased(ntree = 50, mtry = 7))
-model.condForest3 <- party::cforest(trainformula, data = data.train3, 
-                                    controls = party::cforest_unbiased(ntree = 50, mtry = 7))
-model.condForest4 <- party::cforest(trainformula, data = data.train4, 
-                                    controls = party::cforest_unbiased(ntree = 50, mtry = 7))
+                                    controls = party::cforest_unbiased(ntree = 100, mtry = 7))
 
 party::varimp(model.condForest1)
 
 data.pred.condForest1 <- predict(model.condForest1, data.test1, OOB = TRUE, type = "response")
-data.pred.condForest2 <- predict(model.condForest2, data.test2, OOB = TRUE, type = "response")
-data.pred.condForest3 <- predict(model.condForest3, data.test3, OOB = TRUE, type = "response")
-data.pred.condForest4 <- predict(model.condForest4, data.test4, OOB = TRUE, type = "response")
+
+
+# neural networks
+# TODO
+# https://www.r-bloggers.com/fitting-a-neural-network-in-r-neuralnet-package/
+
+# drop all non-numerical features
+match_features_scaled <- match_features %>%
+  dplyr::select(c(8:32))
+
+maxs <- apply(match_features[c(8:32)], 2, max) 
+mins <- apply(match_features[c(8:32)], 2, min)
+
+data.train1.nn.scaled <- as.data.frame(
+  scale(data.train1[c(8:32)], center = mins, scale = maxs - mins))
+data.test1.nn.scaled <- as.data.frame(
+  scale(data.test1[c(8:32)], center = mins, scale = maxs - mins))
+
+formula_nn <- as.formula(paste('outcome',
+                               paste(names(match_features)[c(8:31)],collapse=' + '),
+                               sep=' ~ '))
+
+
+
+formula_nn
+
+model.nn1 <- neuralnet(formula_nn, data = data.train1.nn.scaled, hidden = c(9,3), linear.output = TRUE)
+
+# http://dstath.users.uth.gr/papers/IJRS2009_Stathakis.pdf
+# optimal number of hidden nodes in the first hidden layer is: sqrt[(m+2)N] + 2sqrt[N/(m+2)] 
+# and in the second hidden layer, the optimal number of hidden nodes is: m*sqrt[N/(m+2)], 
+# where N = number of inputs, and m = number of outputs. 
+
+plot(model.nn1)
+
+data.pred.nn1.scaled <- neuralnet::compute(model.nn1, data.test1.nn[c(1:24)])
+data.pred.nn1 <- data.pred.nn1.scaled$net.result * (maxs["outcome"] - mins["outcome"]) + mins["outcome"]
 
 
 # compute evaluation metrics 
 
 metrics.randomForest1.mae <- Metrics::mae(data.test1$outcome, data.pred.randomForest1)
-metrics.randomForest2.mae <- Metrics::mae(data.test2$outcome, data.pred.randomForest2)
-metrics.randomForest3.mae <- Metrics::mae(data.test3$outcome, data.pred.randomForest3)
-metrics.randomForest4.mae <- Metrics::mae(data.test4$outcome, data.pred.randomForest4)
-
 metrics.randomForest1.rmse <- Metrics::rmse(data.test1$outcome, data.pred.randomForest1)
-metrics.randomForest2.rmse <- Metrics::rmse(data.test2$outcome, data.pred.randomForest2)
-metrics.randomForest3.rmse <- Metrics::rmse(data.test3$outcome, data.pred.randomForest3)
-metrics.randomForest4.rmse <- Metrics::rmse(data.test4$outcome, data.pred.randomForest4)
 
 metrics.condForest1.mae <- Metrics::mae(data.test1$outcome, data.pred.condForest1)
-metrics.condForest2.mae <- Metrics::mae(data.test2$outcome, data.pred.condForest2)
-metrics.condForest3.mae <- Metrics::mae(data.test3$outcome, data.pred.condForest3)
-metrics.condForest4.mae <- Metrics::mae(data.test4$outcome, data.pred.condForest4)
-
 metrics.condForest1.rmse <- Metrics::rmse(data.test1$outcome, data.pred.condForest1)
-metrics.condForest2.rmse <- Metrics::rmse(data.test2$outcome, data.pred.condForest2)
-metrics.condForest3.rmse <- Metrics::rmse(data.test3$outcome, data.pred.condForest3)
-metrics.condForest4.rmse <- Metrics::rmse(data.test4$outcome, data.pred.condForest4)
 
-
-metrics.randomForest1.mae
-metrics.randomForest2.mae
-metrics.randomForest3.mae
-metrics.randomForest4.mae
-
-metrics.randomForest1.rmse
-metrics.randomForest2.rmse
-metrics.randomForest3.rmse
-metrics.randomForest4.rmse
+metrics.nn1.mae <- Metrics::mae(data.test1$outcome, data.pred.nn1)
+metrics.nn1.rmse <- Metrics::rmse(data.test1$outcome, data.pred.nn1)
 
 R2 <- function(actual, predicted)
   1 - sum((actual-predicted)^2)/sum((actual-mean(actual))^2)
 
 R2(data.test1$outcome, data.pred.randomForest1)
 R2(data.test1$outcome, data.pred.condForest1)
-
-
-temp <- matches %>% filter(is.na(venue))
-
-temp<-anti_join(matches, API_SP_POP_TOTL_DS2_en_csv_v2, by = c("team1" = "Country Code")) %>%
-  anti_join(API_SP_POP_TOTL_DS2_en_csv_v2, by = c("team1Text" = "Country Name")) %>%
-  group_by(team1Text) %>%
-  summarize(
-    count = n()
-  )
-
-sum(temp$count)
+R2(data.test1$outcome, data.pred.nn1)
+  
